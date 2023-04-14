316f608657aa20b3dcdd1a5d85baf74eed8c082a
Daniil Petrov <octrow@yandex.ru>
2023-04-14 23:00:55 +0600

    v.3.2 -django_auto_prefetching

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django_auto_prefetching import AutoPrefetchViewSetMixin
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -8,7 +7,7 @@ from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
 from posts.models import Group, Post
 
 
-class PostViewSet(AutoPrefetchViewSetMixin, viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
@@ -17,12 +16,12 @@ class PostViewSet(AutoPrefetchViewSetMixin, viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class GroupViewSet(AutoPrefetchViewSetMixin, viewsets.ReadOnlyModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
-class CommentViewSet(AutoPrefetchViewSetMixin, viewsets.ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
